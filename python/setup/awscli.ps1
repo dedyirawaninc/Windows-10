41c2c40128f4ab824b1ac93046220310f0ee3e7d
Dedy Irawan inc. <dedy.irawan.inc@gmail.com>
2025-09-15 22:08:41 +0700

    python setup updates

--- python/setup/awscli.ps1
+++ python/setup/awscli.ps1
@@ -1,18 +1,18 @@
+# Add pyenv-win paths to user PATH environment variable
+$scriptRoot = "$env:USERPROFILE\.pyenv\pyenv-win"
+$scriptPath = "$scriptRoot\Scripts"
+
+pip install awscli --upgrade --user
+python -m site --user-base
+
 # Get current user PATH
 $currentPath = [Environment]::GetEnvironmentVariable("PATH", "User")
 
-pip install awscli --upgrade --user
-
-python -m site --user-base
-
-# Add python scripts paths to user PATH environment variable
-$pythonScripts = "$env:USERPROFILE\AppData\Roaming\Python\Python310\Scripts"
-
-# Add python scripts path if not already present
-if ($currentPath -notlike "*$pythonScripts*") {
-  [Environment]::SetEnvironmentVariable("PATH",$pythonScripts + [Environment]::GetEnvironmentVariable("PATH", "User"),"User")
+# Add shims path if not already present
+if ($currentPath -notlike "*$scriptPath*") {
+  [Environment]::SetEnvironmentVariable("PATH",$scriptPath + [Environment]::GetEnvironmentVariable("PATH", "User"),"User")
 } else {
-  Write-Host "$pythonScripts paths already exist in PATH."
+  Write-Host "$scriptPath paths already exist in PATH."
 }
 
 # If error try to close and reopen vscode

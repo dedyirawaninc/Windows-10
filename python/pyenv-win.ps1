5536c7923473ad5f3c9243b81fa3a89412c68a7b
Dedy Irawan inc. <dedy.irawan.inc@gmail.com>
2025-09-15 16:14:07 +0700

    latest updates

--- python/pyenv-win.ps1
+++ python/pyenv-win.ps1
@@ -1,6 +1,7 @@
 if (Test-Path "./install-pyenv-win.ps1") {
   Remove-Item "./install-pyenv-win.ps1"
 }
+
 Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/pyenv-win/pyenv-win/master/pyenv-win/install-pyenv-win.ps1" -OutFile "./install-pyenv-win.ps1"
 &"./install-pyenv-win.ps1"
 

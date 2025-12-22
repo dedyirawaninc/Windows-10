cc2d433351c54e8c77294f13b52a11c53a3d7eb2
Dedy Irawan inc. <dedy.irawan.inc@gmail.com>
2025-12-22 20:00:09 +0700

    wsl-v2 scripts

--- wsl-v2/dism.ps1
+++ wsl-v2/dism.ps1
@@ -1,5 +1,4 @@
 # DISM (Deployment Image Servicing and Management)
-
 dism /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 dism /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
 dism /online /enable-feature /featurename:HypervisorPlatform /all /norestart

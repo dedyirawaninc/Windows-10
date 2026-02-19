1aa8b13a36373404792d9ce5c987092b0cc16f31
Dedy Irawan inc. <dedy.irawan.inc@gmail.com>
2026-02-18 21:19:24 -0800

    openclaw node

---
+++ openclaw/node/WD11PRO64.ps1
@@ -0,0 +1,3 @@
+$env:OPENCLAW_GATEWAY_TOKEN="4df27d5c3e80a68873b43bc44a7fd1bed4bea33c0c870781"
+openclaw node install --host 192.168.100.107 --port 8789 --display-name "WD11PRO64"
+openclaw node status
